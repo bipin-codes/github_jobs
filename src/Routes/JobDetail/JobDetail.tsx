5e6fcfa871dff32c5c1e58db44bafea514e49b53
bipingosain <bipincodes@outlook.com>
2022-10-24 11:39:44 +0530

    job defail layout added

--- src/Routes/JobDetail/JobDetail.tsx
+++ src/Routes/JobDetail/JobDetail.tsx
@@ -1,4 +1,6 @@
 import ContentBody from "Components/common/ContentBody";
+import LeftContainer from "Components/common/ContentBody/LeftContainer";
+import RightContainer from "Components/common/ContentBody/RightContainer";
 import Header from "Components/common/Header";
 import Heading from "Components/common/Heading";
 import Root from "Components/common/Root";
@@ -10,7 +12,14 @@ const JobDetail = () => {
         <Heading></Heading>
       </Header>
 
-      <ContentBody></ContentBody>
+      <ContentBody>
+        <LeftContainer>
+          <div>Left</div>
+        </LeftContainer>
+        <RightContainer>
+          <div>Right</div>
+        </RightContainer>
+      </ContentBody>
     </Root>
   );
 };
